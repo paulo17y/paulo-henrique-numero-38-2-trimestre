be25940b4580d18a65d855c46d8618e6b14b5c1d
paulo17y <paulo.assis.silva@escola.pr.gov.br>
2025-06-09 19:01:40 -0300

    okkk

--- main.js
+++ main.js
@@ -17,3 +17,26 @@ if (tamanhoSenha < 20){
 tamanhoSenha++;
 }
 numeroSenha.textContent = tamanhoSenha;
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+function geraSenha() {
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
